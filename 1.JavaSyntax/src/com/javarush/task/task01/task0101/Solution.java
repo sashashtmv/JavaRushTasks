168fdcbe4293f38c373d12cee7d2b63e86ce52b3
sashashtmv <sasha-shtmv@rambler.ru>
2018-09-13 00:12:02 +0300

    112

--- 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -6,6 +6,6 @@ package com.javarush.task.task01.task0101;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("sss");//напишите тут ваш код
+        System.out.println("ssss");//напишите тут ваш код
     }
 }
