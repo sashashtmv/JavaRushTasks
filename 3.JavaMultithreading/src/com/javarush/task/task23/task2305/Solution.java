3a5bd387cf3cea941ac8e9b207d1a68820966f89
Alexandr Shtomov <sashashtmv06@gmail.com>
2018-10-15 23:42:46 +0300

    exp11

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task23.task2305;
+
+/* 
+Inner
+*/
+public class Solution {
+    public InnerClass[] innerClasses = new InnerClass[2];
+
+    public class InnerClass {
+    }
+
+    public static Solution[] getTwoSolutions() {
+        Solution[] sol = new Solution[2];
+        sol[0] = new Solution();
+        sol[0].innerClasses[0] = new Solution().new InnerClass();
+        sol[0].innerClasses[1] = new Solution().new InnerClass();
+        sol[1] = new Solution();
+        sol[1].innerClasses[0] = new Solution().new InnerClass();
+        sol[1].innerClasses[1] = new Solution().new InnerClass();
+        return sol;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
